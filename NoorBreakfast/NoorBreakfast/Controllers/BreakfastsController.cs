bc70400bdfd86af9a4bac42c7cedb809ab3ceb35
Tanvir Ahmed <61121499+TanvirAhmed1@users.noreply.github.com>
2022-11-07 18:48:06 +0600

    Controller Added

---
+++ NoorBreakfast/NoorBreakfast/Controllers/BreakfastsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NoorBreakfast.Contracts.Breakfast;
+
+namespace NoorBreakfast.Controllers
+{
+    [ApiController]
+    public class BreakfastsController : ControllerBase
+    {
+        [HttpPost("/breakfasts")]
+        public IActionResult CreateBreakfast(CreateBreakfastRequest request)
+        {
+            return Ok(request);
+        }
+    }
+}
